f41e1835f1edc821e970ced7717a6006709e2dc7
unknown <leander.maben@gmail.com>
2020-03-06 00:28:15 +0530

    Initial Frontend Commit

---
+++ frontend/HotelManagement/src/application/HomeController.java
@@ -0,0 +1,25 @@
+package application;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class HomeController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button loginbtn;
+
+    @FXML
+    private TextField userid;
+
+    @FXML
+    void login() {
+    	
+    }
+
+}
---
+++ frontend/HotelManagement/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("AdminPage.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.initStyle(StageStyle.DECORATED);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
